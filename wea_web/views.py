649efb2e2c644de49605d4a2aab38db28fcecb59
huanglun1994 <492593760@qq.com>
2017-09-21 15:02:59 +0800

    commit

--- wea_web/views.py
+++ wea_web/views.py
@@ -3,6 +3,7 @@ from wea_web.models import Weather
 
 
 def index(request):
-    weather = Weather.objects.all()
+    pk = Weather.objects.last().id
+    weather = Weather.objects.all()[pk-7:]
     context = {'weather': weather}
     return render(request, 'index.html', context=context)
