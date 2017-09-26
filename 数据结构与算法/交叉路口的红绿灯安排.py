05839f6a51651ec03084c129b2be5e80d84a80eb
huanglun1994 <492593760@qq.com>
2017-09-26 11:12:27 +0800

    交叉路口的红绿灯安排

--- 数据结构与算法/交叉路口的红绿灯安排.py
+++ 数据结构与算法/交叉路口的红绿灯安排.py
@@ -1,3 +1,19 @@
 # -*- coding: utf-8 -*-
-"""xxxxx"""
+"""交叉路口的红绿灯安排"""
 __author__ = 'Huang Lun'
+
+
+def coloring(G):  # 做图G的着色
+    color = 0
+    groups = set()
+    verts = vertices(G)  # 取得G的所有顶点，依赖于图的表示
+    while verts:  # 如果集合不空
+        new_group = set()
+        for v in list(verts):
+            if not_adjacent_with_set(v, new_group, G):
+                new_group.add(v)
+                verts.remove(v)
+
+        groups.add((color, new_group))
+        color += 1
+    return groups
