cf59938199a735f87321d20dc947d10f17cfcf5f
huanglun1994 <492593760@qq.com>
2017-09-26 14:58:21 +0800

    斐波那契数列

--- 数据结构与算法/斐波那契数列.py
+++ 数据结构与算法/斐波那契数列.py
@@ -9,3 +9,11 @@ def fib(n):
         return 1
     else:
         return fib(n-1) + fib(n-2)
+
+
+def fib1(n):
+    """用递推算法写一个斐波那契数列"""
+    f1 = f2 = 1
+    for k in range(1, n):
+        f1, f2 = f2, f2 + f1
+    return f2
