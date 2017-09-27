3f4c300f3390ea69e296ab685c2de411492c06fa
huanglun1994 <492593760@qq.com>
2017-09-27 13:20:17 +0800

    有理数的加法实现

--- 数据结构与算法/有理数的加法实现.py
+++ 数据结构与算法/有理数的加法实现.py
@@ -2,18 +2,18 @@
 """有理数的加法实现"""
 __author__ = 'Huang Lun'
 
-# 一个有理数3/5的存储方式如下
-a1 = 3
-b1 = 5
+# 用一个python元组表示一个有理数，约定其中的第0项表示分子，第1项表示分母
+r1 = (3, 5)
+r2 = (7, 10)
 
 
-def rational_plus(a1, b1, a2, b2):
+def rational_plus(r1, r2):
     """
     有理数的加法定理为：
-    (a1*b2+b1*a2)/(b1*b2)
+    r1的分子乘以r2的分母与r1的分母乘以r2的分子之和除以r1与r2的分母之积
     """
-    num = (a1 * b2) + (b1 * a2)
-    den = b1 * b2
+    num = (r1[0] * r2[1]) + (r1[1] * r2[0])
+    den = r1[1] * r2[1]
     return num, den
 
-print(rational_plus(a1, b1, 7, 10))
+print(rational_plus(r1, r2))
